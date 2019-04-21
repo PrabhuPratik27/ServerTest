9e31546a6cb8d980efb749e87c28121b30979c3c
Pratik <prabhupratik27@gmail.com>
2019-04-21 19:54:09 +0530

    Changed port

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,4 @@
 const AuthController = require('./controllers/AuthController')
-const addEvent = require('./controllers/addEvent')
-const upload = require('./controllers/posterUpload')
-const Glimpsesupload = require('./config/multerglimpsesconfig')
-const GlimpseController = require('./controllers/GLimpseController')
 
 module.exports = (app) => {
     app.post('/register',AuthController.register)
